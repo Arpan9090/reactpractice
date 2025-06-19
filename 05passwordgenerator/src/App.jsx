480de9ddbcade1ec832a5e5c97a92cdd704f0729
Arpan Maurya <thearpanmaurya@gmail.com>
2025-06-19 23:52:23 +0530

    passwordGenerator

---
+++ 05passwordgenerator/src/App.jsx
@@ -0,0 +1,53 @@
+import { useCallback, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (numAllowed) str += "0123456789";
+    if (charAllowed) str += "@#$!&";
+
+    for (i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numAllowed, charAllowed, setPassword]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto rounded-lg px-4 my-8 text-orange-500 bg-gray-700  ">
+        <h1 className="text-white text-center text-4xl">Password Generator</h1>
+        <div className="flex justify-center shadow rounded-lg overflow-hidden py-2">
+          <input
+            type="text"
+            value={password}
+            placeholder="password"
+            readOnly
+            className="outline-none w-full py-1 px-3 bg-white rounded-lg"
+          />
+          <button>copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex item-center gap-x-1">
+            <input type="range" min={8} max={24} value={length} className="cursor-pointer" onChange={(e) => {setLength(e.target.value)}} />
+            <label htmlFor="">Length:{length}</label>
+            <input type="checkbox" name="" id="" defaultChecked={numAllowed} onChange={setNumAllowed((prev) => !prev)}/>
+            <label htmlFor="">Number</label>
+            <input type="checkbox" name="" id="" defaultChecked={charAllowed} onChange={setCharAllowed((prev) => !prev)}/>
+            <label htmlFor="">Character</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
